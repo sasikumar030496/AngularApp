d2ef416808fe41a5fbaa56fb9ddaccf5e3134c6a
Sasi Kumar <sasikumar030496@gmail.com>
2024-01-09 17:38:15 +0530

    Angular till Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'AppComponentSelector',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularApp';
+  title = 'AngularApp'
+  company = 'Accenture';
 }
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'WelcomeComponentSelector',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent {
+        company : string = "Accenture India"
+        buttonFlag : boolean = false;
+        userValue : string = ""
+        showMessage(){
+          this.company = this.userValue
+        }
+}
